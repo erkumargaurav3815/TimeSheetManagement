c8aa32d46acf4d1eeb8b9adf73d5d567ab9aaf6f
erkumargaurav3815 <kumar.gaurav@habilelabs.io>
2026-07-12 19:59:35 +0530

    Updated App.tsx App.css index.css main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import AppBar from "./Components/AppBar";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppBar />
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
