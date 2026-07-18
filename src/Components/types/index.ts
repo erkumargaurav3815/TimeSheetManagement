fa73cb3316e5a9ea006fbdd1e741895c1d5d703d
erkumargaurav3815 <kumar.gaurav@habilelabs.io>
2026-07-19 01:13:07 +0530

    Added component types

---
+++ src/Components/types/index.ts
@@ -0,0 +1,11 @@
+export interface Task {
+  id: number;
+  category: string;
+  name: string;
+  description: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  timeTaken: string;
+  status: string;
+}
